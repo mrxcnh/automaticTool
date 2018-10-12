3fc23c3c4c5fbdea7b15a1395807c986a3727121
ThanhDV <thanhdovan210184@gmail.com>
2018-10-12 11:20:04 +0700

    add download .doc & .docx file

--- downloadXML.py
+++ downloadXML.py
@@ -8,15 +8,20 @@ link = []
 for x in linksize:
     link.append(x.get_attribute("href"))
 
-browser.close()
+for x in link:
+    if x.endswith("docx") or x.endswith("doc"):
+        browser.get(x)
 
 download_dir="/home/wnncr/Downloads"
 options = webdriver.ChromeOptions()
 profile = {"plugins.plugins_list": [{"enabled": False, "name": "Chrome PDF Viewer"}],"download.default_directory": download_dir , "download.extensions_to_open": "applications/pdf"}
 options.add_experimental_option("prefs", profile)
+
+
 for i in link:
     if i.endswith('pdf'):
         driver = webdriver.Chrome('/usr/bin/chromedriver', chrome_options=options)
         driver.get(i)
 
+browser.close()
 driver.close()
